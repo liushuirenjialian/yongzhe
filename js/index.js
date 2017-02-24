8b5f772445d71dd38588ab9ccc6c52fb83fe7143
heyanfang <heyanfang@hoolai.com>
2017-02-24 15:13:39 +0800

    s

---
+++ js/index.js
@@ -0,0 +1,42 @@
+$(function() {
+    var gp = $(".game-info>p");
+    var arrow = gp.next();
+    if (gp.height() > 48) {
+        gp.css({
+            // "height" : "48px",
+            // "-webkit-line-clamp" : "2"
+        });
+        arrow.show();
+    }
+    var flag = true;
+    arrow.click(function() {
+        if (flag) {
+            gp.attr("style", "height: auto;")
+            arrow.addClass("active");
+        } else {
+            gp.attr("style", "height: 48px;-webkit-line-clamp:2");
+            arrow.removeClass("active");
+        }
+        flag = !flag;
+    });
+
+    // $('.img-content').width( ( $('img').width()+1)*$('img').length)
+    $(".img-content").width(($(".img-container img").width() + 4) * $(".img-container img").length);
+    $(".fuli1 img").click(function(e) {
+        $(".swiper").show();
+        var mySwiper = new Swiper('.swiper-container', {
+            //autoplay:3000,//可选选项，自动滑动
+            pagination: '.swiper-pagination',
+            loop: true,
+            autoplayDisableOnInteraction: false,
+        });
+        mySwiper.slideTo(e.target._zid - 1);
+    });
+
+    $(".swiper").click(function() {
+        $('.swiper').hide();
+    });
+    $(".swiper-container").click(function(e) {
+        e.stopPropagation();
+    });
+});
